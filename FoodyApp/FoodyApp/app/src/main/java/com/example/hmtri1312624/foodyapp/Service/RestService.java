5ea322c8d554d92d881474eabc1e9768514179a4
milotiger <black.thanhtri@gmail.com>
2016-05-29 19:18:00 +0700

    Set timeout for connections

--- FoodyApp/FoodyApp/app/src/main/java/com/example/hmtri1312624/foodyapp/Service/RestService.java
+++ FoodyApp/FoodyApp/app/src/main/java/com/example/hmtri1312624/foodyapp/Service/RestService.java
@@ -2,6 +2,9 @@ package com.example.hmtri1312624.foodyapp.Service;
 
 import com.example.hmtri1312624.foodyapp.Interface.APIService;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,17 +13,24 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class RestService {
-    private static final String URL = "http://192.168.1.5/";
+    private static final String URL = "http://whattoeattoday.gear.host/";
     private Retrofit restAdapter;
     private APIService apiService;
 
     public RestService()
     {
 
+        final OkHttpClient client = new OkHttpClient();
+        OkHttpClient clientWith30sTimeOut = client.newBuilder()
+                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+
         restAdapter = new Retrofit.Builder()
                 .baseUrl(URL)
                 .addConverterFactory(GsonConverterFactory.create())
-                //.setLogLevel(retrofit.RestAdapter.LogLevel.FULL)
+                .client(clientWith30sTimeOut)
                 .build();
 
         //Create an instance of the Service interface that we created from the Buider above
